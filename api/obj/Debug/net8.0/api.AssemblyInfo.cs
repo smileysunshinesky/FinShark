985621cd939eea26181a589445388f5246ba2a2b
sunshine0214 <danieleverest920@gmail.com>
2024-03-03 16:40:33 -0800

    pagination
    Co-Authored-By: Daniel <151569262+danieleverest@users.noreply.github.com>
    Co-Authored-By: Edward Thomas <159745579+edward7770@users.noreply.github.com>

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1778f0d85f8dd90263e7c1fa256af00f1ed90b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f3805c13a09fc62211c10399c2c0337a3887c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
