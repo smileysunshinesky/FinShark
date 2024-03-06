11e8629257bfaac4ec13c9c6d9d1bc38a3920e98
sunshine0214 <danieleverest920@gmail.com>
2024-03-05 22:09:58 -0800

    Token
    Co-Authored-By: Edward Thomas <159745579+edward7770@users.noreply.github.com>

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+601a6cd53873e6743730ce8c9d97d9b9b5421468")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7536dc65db6a05d62f21cfd51715004abee1444")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
