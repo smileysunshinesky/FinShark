2e0bb79b8842cf03701b90f566b13ce66210187a
sunshine0214 <danieleverest920@gmail.com>
2024-03-10 20:46:19 -0700

    Comment Box, comment post
    Co-Authored-By: Daniel <151569262+danieleverest@users.noreply.github.com>
    Co-Authored-By: Dev Star✨ <159745579+edward7770@users.noreply.github.com>

--- frontend/src/Components/CompanyProfile/CompanyProfile.tsx
+++ frontend/src/Components/CompanyProfile/CompanyProfile.tsx
@@ -5,6 +5,7 @@ import { getKeyMetrics } from '../../api';
 import RatioList from '../RatioList/RatioList';
 import Spinner from '../Spinner/Spinner';
 import { formatLargeNonMonetaryNumber, formatRatio } from '../../Helpers/NumberFormatting';
+import StockComment from '../StockComment/StockComment';
 
 type Props = {}
 
@@ -93,6 +94,7 @@ const CompanyProfile = (props: Props) => {
     { companyData? (
       <>
       <RatioList data={companyData} config={tableConfig}/>
+      <StockComment stockSymbol={ticker} />
       </>
     ): (
       <Spinner />
